bf857776a3929edde9ff10f8d407ec0a52270970
Hariharan <subhash@constient.com>
2020-02-12 20:05:43 +0530

    node - enable cors origin issue fixing code

--- graphql-test-backend-nodejs/app.js
+++ graphql-test-backend-nodejs/app.js
@@ -9,32 +9,32 @@ app.use(express.json({ limit: '50mb' }));
 
 app.use("/images", express.static(path.join(__dirname, 'images')));
 app.all('*', function (req, res, next) {
-  // var allowedOrigins = CONFIG.allowedOrigins  //allowed origins only allowed to access
-  // var origin = req.headers.origin;
-  // console.log(origin)
-  // console.log(allowedOrigins)
+  var allowedOrigins = CONFIG.allowedOrigins  //allowed origins only allowed to access
+  var origin = req.headers.origin;
+  console.log(origin)
+  console.log(allowedOrigins)
 
-  // if (allowedOrigins.indexOf(origin) > -1) {
-  //   res.setHeader('Access-Control-Allow-Origin', origin);
-  // } else {
-  //   return;
-  // }
-  // var responseSettings = {
-  //   "AccessControlAllowOrigin": req.headers.origin,
-  //   "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, X-File-Name",
-  //   "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
-  //   "AccessControlAllowCredentials": true
-  // };
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  } else {
+    return;
+  }
+  var responseSettings = {
+    "AccessControlAllowOrigin": req.headers.origin,
+    "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, X-File-Name",
+    "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+    "AccessControlAllowCredentials": true
+  };
 
-  // res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
-  // res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
-  // res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
-  // if ('OPTIONS' == req.method) {
-  //   res.send(200);
-  // }
-  // else {
+  res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+  res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+  res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
     next();
-  // }
+  }
 });
 // Graph-QL configuration
 const graphqlUploadExpress = require('graphql-upload');
